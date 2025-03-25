b9b1081a55dcf20a2a7343284ab26d200a47a9f2
Nazima Ergeshova <ergeshovanazima1@gmail.com>
2025-03-26 00:18:10 +0900

    3월 26일 Programmers Level 1 문제 1 코드 추가

---
+++ programmers/level1_problem1.java
@@ -0,0 +1,9 @@
+
+public class level1_problem1 {
+    public static void main(String[] args) {
+        String message = "";
+
+        System.out.println("321");
+        System.out.println(message);
+    }
+}
